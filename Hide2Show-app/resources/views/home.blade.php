abc29c2d215b44e1013ce05829506fb02c5b8aee
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2024-06-01 21:53:49 +0700

    Feat: Page Upload & Download

--- Hide2Show-app/resources/views/home.blade.php
+++ Hide2Show-app/resources/views/home.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>HALO</h1>
+    <div class="d-flex justify-content-evenly mt-5">
+        <h1 class="fw-bold">Encrypt</h1>
+        <h1 class="fw-bold">Decrypt</h1>
+    </div>
+    <div class="input-group mb-3 mt-5 ml-5">
+        <label class="input-group-text" for="inputGroupFile01">Upload</label>
+        <input type="file" class="form-control" id="inputGroupFile01">
+    </div>
 @endsection
